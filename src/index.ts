28870408723c5ba24f72261603c84e6452e46337
Alex Kaminsky <akaminsky@azavea.com>
2020-10-09 16:29:24 -0400

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-export * from './lib/number';
+import * as GeoJSON from 'geojson';
+import polygonClipping, { Geom } from 'polygon-clipping';
+
+export default (
+  feature:
+    | GeoJSON.Feature<GeoJSON.MultiPolygon>
+    | GeoJSON.Feature<GeoJSON.Polygon>
+) => {
+  const featureCoords = feature.geometry.coordinates as Geom;
+  const worldCoords: Geom = [
+    [
+      [
+        [180, 90],
+        [-180, 90],
+        [-180, -90],
+        [180, -90],
+        [180, 90],
+      ],
+    ],
+  ];
+  return {
+    type: 'Feature',
+    geometry: {
+      type: 'MultiPolygon',
+      coordinates: polygonClipping.difference(worldCoords, featureCoords),
+    },
+  };
+};
